d702773c104bf8f0f3c9512a1e6743f2241f4706
Costin V Cozianu <cocozianu@fb.com>
2021-01-20 19:56:15 -0800

    Coverage 100%

--- src/main/java/ztestlibforjenkinsintegration/dummy/FancyLambda.java
+++ src/main/java/ztestlibforjenkinsintegration/dummy/FancyLambda.java
@@ -3,7 +3,11 @@ package ztestlibforjenkinsintegration.dummy;
 import java.util.function.Function;
 
 public class FancyLambda {
-
+	private FancyLambda() {	}
+	static {
+		new FancyLambda();
+	}
+	
 	public static Function<String, String> withPrefix(String prefix) {
 		var dot= ".";
 		return (s -> prefix + dot + s);
